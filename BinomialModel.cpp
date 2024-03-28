4cb47a60f7c76beff8a1a0b83a5ae4ac002e2bea
dkp116 <daneel668@gmail.com>
2024-03-28 15:26:27 +0000

    Class BinomialModel
    Created a class for Binomial Model that has member functions to allow inputs and validate the data

---
+++ BinomialModel.cpp
@@ -0,0 +1,51 @@
+#include "BinomialModel.h"
+#include<cmath>
+
+
+ double BinModel::GetR(){return R;}
+
+ double BinModel::InputValues()
+ {
+
+//Enter Data
+    cout<< "Enter S0"; cin>> S0;
+    cout<< "Enter U"; cin>> U;
+    cout<< "Enter D"; cin>> D;
+    cout<< "Enter R"; cin>> R;
+
+//Validate data
+    
+    if(S0<=0.0||U<=-1.0 || D<=-1.0 || R <=-1.0)
+    {   cout<< "Illegal Data range"<< endl;
+        return 1;}
+
+//Check for Arbritrage
+
+    if(U<R ||D>R)
+        {
+            cout << "Arbritriage detected" << endl;
+            return 1;
+        
+        }
+
+    cout << "Input Data has been checked \n No Arbritrage" << endl;
+
+
+ }
+
+
+//Find RNP
+ double BinModel::RiskNeutralP()
+{
+
+    return (R-D)/(U-D);
+
+ }
+
+//Find Stock Price with i Up steps over n periods
+
+  double BinModel::S(int n, int i)
+  {
+    return S0*pow(1+U,i)*pow(1+D,n-i);
+    
+  }
---
+++ BinomialModel.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#define BinomialModel_h
+using namespace std;
+
+class BinModel {
+
+    private: 
+    double S0;
+    double U;
+    double D;
+    double R;
+
+    public: 
+
+    double GetR(); //Return R value
+
+    double InputValues();  //Get input data for S0,U,D,R
+
+    double RiskNeutralP(); //Find RNP
+
+    double S(int n, int i); //Compute stock price 
+
+    
+};
+
